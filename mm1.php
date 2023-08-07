e0654143e443c4cff034851a93c0dc7aa92cadfe
skumar <141522297+sndh7@users.noreply.github.com>
2023-08-08 02:55:55 +0530

    Add files via upload

---
+++ mm1.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>   
+<title>INDEX PAGE</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/db71/gradientbg.css">
+  <link href="/img/icon256/logo1.png" rel="icon">
+  <link href="/img/icon256/logo1.png" rel="apple-touch-icon">
+  <base href="https://patiltechnicalsolutions.com">
+<style>
+body{max-width:600px; margin:1px auto;}
+a:link, a:visited, a:active {color:#0066ff;} a:hover{color:red;} 
+.sdiv1{border-radius:15px 0 0 15px;}
+.sdiv2{border-radius:0 15px 15px 0;}
+.sdiv3{border:2px solid white; border-radius:10px; background: linear-gradient(to top left, #33ccff 0%, #ff99cc 100%); box-shadow:0 6px 10px 0 rgba(0, 0, 0, 0.7); }
+.sdiv3:hover{background: linear-gradient(to bottom right, #00ff99 0%, #ffffff 100%);}
+.active{background: linear-gradient(to bottom right, #00ff99 0%, #ffffff 100%);}
+
+@media only screen and (max-width: 600px) {
+body {font-size:16px; margin-top:0;}
+.sdiv1{border-radius:15px 15px 0 0;}
+.sdiv2{border-radius:0 0 15px 15px;}
+}
+
+.chotu {
+background-color: linear-gradient(to bottom right, #00ff99 0%, #ffffff 100%); 
+border:2px solid white; 
+padding:5px;
+border-radius:7px;
+width:57px;
+margin:7px 5px;
+}
+
+.sdh2{
+background:linear-gradient(44deg, #0431a5, #116d68);
+padding:5px; border-radius:5px; width:325px; color:white;
+font-family:arial; margin:5px;
+}
+
+</style>
+
+</head>
+
+<body class="bg31">
+<center><h2 class="sdh2">Your Website Name</h2></center>
+
+<center>
+    
+<a href="index.php"><img src="/img/icon256/home.png" class="chotu sdiv3 active" title="HOME"></a>
+<a href="users.php"><img src="/img/icon256/commu8b.png" class="chotu sdiv3" title="SINGLE USER INFO"></a>
+<a href="index2.php"><img src="/img/icon256/comment.png" class="chotu sdiv3" title="POST"></a>
+<a href="usersinfo.php"><img src="/img/icon256/social2.png" class="chotu sdiv3" title="USERS INFO"></a>
+<a href="index3.php"><img src="/img/icon256/msgall.png" class="chotu sdiv3" title="GROUP MESSEGE"></a>
+
+<a href="#"><img src="/img/icon256/upload.png" class="chotu sdiv3" title="#"></a>
+<a href="#"><img src="/img/sdtech2.png" class="chotu sdiv3" title="#"></a>
+<a href="#"><img src="/img/doc2.png" class="chotu sdiv3" title="#"></a>
+<a href="#"><img src="/img/favicon3.png" class="chotu sdiv3" title="#"></a>
+
+<a href="#"><img src="/img/icon256/target.png" class="chotu sdiv3" title="#"></a>
+<a href="#"><img src="/img/icon256/gallery.png" class="chotu sdiv3" title="#"></a>
+<a href="#"><img src="/img/icon256/links.png" class="chotu sdiv3" title="#"></a>
+<a href="#"><img src="/img/icon256/group3p.png" class="chotu sdiv3" title="#"></a>
+
+<a href="#"><img src="/img/icon256/openbook2.png" class="chotu sdiv3" title="#"></a>
+<a href="#"><img src="/img/icon256/chatbot.png" class="chotu sdiv3" title="#"></a>
+<a href="#"><img src="/img/icon256/group.png" class="chotu sdiv3" title="#"></a>
+<a href="#"><img src="/img/icon256/testi5.png" class="chotu sdiv3" title="#"></a>
+<a href="#"><img src="/img/icon256/email.png" class="chotu sdiv3" title="#"></a>
+<a href="#"><img src="/img/icon256/handshake.png" class="chotu sdiv3" title="#"></a>
+<a href="#"><img src="/img/icon256/learner.png" class="chotu sdiv3" title="#"></a>
+<a href="#"><img src="/img/icon256/customerservice.png" class="chotu sdiv3" title="#"></a>
+<a href="#"><img src="/img/icon256/partners.png" class="chotu sdiv3" title="#"></a>
+
+
+
+
+
+
+<a href="/img/icon256/settings.php"><img src="/img/icon256/service.png" class="chotu sdiv3" title="SETTINGS"></a>
+<a href="#"><img src="/img/icon256/socialmd.png" class="chotu sdiv3" title="#"></a>
+<a href="#"><img src="/img/icon256/sm_whatsapp3.png" class="chotu sdiv3" title="#"></a>
+<a href="#"><img src="/img/icon256/phonee.png" class="chotu sdiv3" title="MAIL TO"></a>
+<a href="#"><img src="/img/icon256/place.png" class="chotu sdiv3" title="#"></a>
+<a href="/img/icon256/logout.php"><img src="/img/email5.png" class="chotu sdiv3" title="LOGOUT PAGE"></a>
+</center>
+
+<br>
+
+</body>
+</html> 
---
+++ mm2.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://patiltechnicalsolutions.com/db71/gradientbg.css">
+<link href="https://patiltechnicalsolutions.com/img/icon256/united.png" rel="icon">
+<link href="https://patiltechnicalsolutions.com/img/icon256/united.png" rel="apple-touch-icon">
+<style>
+body{max-width:625px; margin:1px auto;}
+
+.container {
+  position: relative;
+  text-align: center;
+  color: black;
+  float:left;
+  margin:2px 2px 18px 2px; width:85px; height:85px;
+  }
+  
+.centered {
+  position: absolute;
+  top: 98%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background:#ffff99; padding:3px 5px; border-radius:3px;
+  font-family:arial; font-weight:bold; border:1px solid black;
+}
+
+.sdimg {
+border-radius:9px; 
+width:63px; height:63px; border:2px solid white; 
+box-shadow:0 6px 10px 0 rgba(0, 0, 0, 0.7); 
+background:linear-gradient(to top left, #33ccff 0%, #ff99cc 100%); padding:7px;}
+.sdimg:hover{background:linear-gradient(to top left, #00ff99 0%, #ffffff 100%);}
+
+a:link, a:visited, a:active {color:#000;} a:hover{color:red;} 
+.active{background: linear-gradient(to bottom right, #00ff99 0%, #ffffff 100%);}
+
+.sdh2{
+background:linear-gradient(44deg, #0431a5, #116d68);
+padding:5px; border-radius:5px; width:325px; color:white;
+font-family:arial; margin:5px;
+}
+
+</style>
+</head>
+
+<body class="bg31">
+
+<center><h2 class="sdh2">Your Website Name</h2></center>
+
+<div class="container"><a href="/">
+<img src="https://patiltechnicalsolutions.com/img/webhome10.png" class="sdimg active" >
+<div class="centered">Home</div>
+</a></div>
+
+<div class="container"><a href="/">
+<img src="https://patiltechnicalsolutions.com/img/group.png" class="sdimg" >
+<div class="centered">About</div></a></div>
+
+<div class="container"><a href="/">
+<img src="https://patiltechnicalsolutions.com/img/menu11.png" class="sdimg" >
+<div class="centered">Services</div></a></div>
+
+<div class="container"><a href="/">
+<img src="https://patiltechnicalsolutions.com/img/notes2.png" class="sdimg" >
+<div class="centered">Profile</div></a></div>
+
+<div class="container"><a href="/">
+<img src="https://patiltechnicalsolutions.com/img/people3.png" class="sdimg" >
+<div class="centered">Blog</div></a></div>
+
+<div class="container"><a href="/">
+<img src="https://patiltechnicalsolutions.com/img/people.png" class="sdimg" >
+<div class="centered">Social</div></a></div>
+
+<div class="container"><a href="/">
+<img src="https://patiltechnicalsolutions.com/img/company2.png" class="sdimg" >
+<div class="centered">Employ</div></a></div>
+
+<div class="container"><a href="/">
+<img src="https://patiltechnicalsolutions.com/img/people2.png" class="sdimg" >
+<div class="centered">Latest</div></a></div>
+
+<div class="container"><a href="/">
+<img src="https://patiltechnicalsolutions.com/img/person2.png" class="sdimg" >
+<div class="centered">Carrier</div></a></div>
+
+<div class="container"><a href="/">
+<img src="https://patiltechnicalsolutions.com/img/logo.png" class="sdimg" >
+<div class="centered">Digital</div></a></div>
+
+<div class="container"><a href="/">
+<img src="https://patiltechnicalsolutions.com/img/folder.png" class="sdimg" >
+<div class="centered">Download</div></a></div>
+
+<div class="container"><a href="/">
+<img src="https://patiltechnicalsolutions.com/img/settings3.png" class="sdimg" >
+<div class="centered">Program</div></a></div>
+
+<div class="container"><a href="/">
+<img src="https://patiltechnicalsolutions.com/img/service.png" class="sdimg" >
+<div class="centered">Training</div></a></div>
+
+<div class="container"><a href="/">
+<img src="https://patiltechnicalsolutions.com/img/locked2.png" class="sdimg" >
+<div class="centered">Login</div></a></div>
+
+<div class="container"><a href="/">
+<img src="https://patiltechnicalsolutions.com/img/msg2.png" class="sdimg" >
+<div class="centered">Information</div></a></div>
+
+<div class="container"><a href="/">
+<img src="https://patiltechnicalsolutions.com/img/contactus5a.png" class="sdimg">
+<div class="centered">Contact</div></a></div>
+
+
+</body>
+</html> 
